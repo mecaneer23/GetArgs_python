b3202c42ca9585f7dd930e2e07d63d32041c9715
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2021-09-04 22:38:14 -0500

    help message fix - user can now change manually

--- getargs.py
+++ getargs.py
@@ -1,24 +1,20 @@
 import sys
 
-help_message = f"USAGE:\n{sys.argv[0]} [OPTIONS]\n\n--help\t\t-h\t\tDisplays this message\n"
+help_message = f"USAGE:\n{sys.argv[0]} [OPTIONS]\n\n"
 
 def help_function():
 	global help_message
 	print(help_message)
 
-functions = {
-	"help": help_function
-}
-shorthands = {
-	"h": "help"
-}
+shorthands = {}
+info = {}
 
 def error(message):
 	print(f"ERROR: {message}")
 	sys.exit(1)
 
 def add_argument(argument: str, shorthand: str=None, function=help_function, description: str=None):
-	global functions, shorthands, help_message
+	global info, shorthands
 	if not argument.isalpha():
 		error("Arguments can only consist of letters")
 	elif len(shorthand) > 1:
@@ -26,11 +22,14 @@ def add_argument(argument: str, shorthand: str=None, function=help_function, des
 	elif description == None:
 		error("For usablility, make sure to include a description. If you don't want a description, set description=\"\".")
 	else:
-		functions[argument] = function
 		shorthands[shorthand] = argument
-		help_message += f"--{argument}    \t-{shorthand}\t\t{description}\n"
+		info[argument] = [function, description]
 
 def handle_args():
+	global help_message
+	descs = [info[i][1] for i in info]
+	for arg, short, desc in zip(info, shorthands, descs):
+		help_message += f"--{arg}    \t-{short}\t\t{desc}\n"
 	for i, v in enumerate(sys.argv):
 		if v[0:2] == "--":
 			if len(v) >= 2:
@@ -42,9 +41,11 @@ def handle_args():
 						elif v[idx].isalpha() and v[idx+1] == "=":
 							v[2:idx]
 				else:
-					functions[v[2:]]()
+					info[v[2:]][0]()
 			else:
 				error(f"Length of argument must be greater than zero")
 		elif v[0] == "-":
 			for value in v[1:]:
-				functions[shorthands[value]]()
+				info[shorthands[value]]()
+
+add_argument("help", "h", help_function, "Displays this message")
